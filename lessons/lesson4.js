70ab5ea56a4e6fe18f13dc178ab6d4053183fc0b
timotej <paptimotej@gmail.com>
2025-08-27 10:48:05 +0200

    Greater than

---
+++ lessons/lesson4.js
@@ -0,0 +1,9 @@
+// Relational or Comparison Operators
+
+// > - Greater than
+// < - Less than
+// >= - Greater than or equal to
+// <= - Less than or equal to
+
+var result = 5 > 3; // true
+console.log(result);
