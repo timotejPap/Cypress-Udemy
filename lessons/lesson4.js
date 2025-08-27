b34d6b1ede6b606cb04f01405ff2da46d410a35e
timotej <paptimotej@gmail.com>
2025-08-27 11:15:16 +0200

    Relational or Comparison Operators

--- lessons/lesson4.js
+++ lessons/lesson4.js
@@ -5,5 +5,20 @@
 // >= - Greater than or equal to
 // <= - Less than or equal to
 
-var result = 5 > 3; // true
-console.log(result);
+var result1 = 5 > 3; // true
+console.log(result1);
+
+var result2 = 5 < 3; // false
+console.log(result2);
+
+var result3 = 5 >= 5; // true
+console.log(result3);
+
+var result4 = 5 <= 4; // false
+console.log(result4);
+
+// equality operators
+
+var x = 10; // number
+console.log(x == "10"); // true (loose comparison, type coercion)
+console.log(x === "10"); // false (strict comparison, no type coercion) here '===' checks both value and type
