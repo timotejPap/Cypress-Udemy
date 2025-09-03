3710729df2eeabdfac44b939e088a46f9038a20e
Timotej Pap <paptimotej@gmail.com>
2025-09-03 09:56:26 +0200

    conditional statements

---
+++ lessons/lesson6.js
@@ -0,0 +1,34 @@
+// Conditional Statements
+
+// if (condition) {
+//     // code to be executed if condition is true
+// } else {
+//     // code to be executed if condition is false
+// }
+
+// If hour between 6 and 12: "Good morning!"
+// If hour between 12 and 18: "Good afternoon!"
+// Otherwise: "Good evening!"
+
+// var hour = 12
+
+// if (hour >= 6 && hour < 12) {
+//     console.log("Good morning!")
+// }
+// else if (hour >= 12 && hour < 18) {
+//     console.log("Good afternoon!")
+// }
+// else {
+//     console.log("Good evening!")
+// }
+
+
+var ageIsMoreThan18 = false
+var isUSCitizen = true
+
+if (ageIsMoreThan18 && isUSCitizen) {
+    console.log("This customer is eligible for driving license")
+}
+else {
+    console.log("This customer is NOT eligible for driving license")
+}
