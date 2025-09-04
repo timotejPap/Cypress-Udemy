aa9867a446d9f734eb8136b6faecb2dda75d44bc
Timotej Pap <paptimotej@gmail.com>
2025-09-04 11:08:16 +0200

    renaming the files

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,26 @@
 export function printAge(age) {
     console.log("Age: " + age);
 }
+
+
+
+// Class with methods
+
+export class CustomerDetails {
+    /**
+     * This method prints the user´s first name
+     * @param {string} firstName
+     */
+    printFirstName(firstName) {
+        console.log(firstName);
+    }
+
+
+    /** //   /**
+     * This method prints the user´s last name
+     * @param {string} lastName 
+     */
+    printLastName(lastName) {
+        console.log(lastName);
+    }
+}
---
+++ lessons/9classMethods.js
@@ -0,0 +1,5 @@
+import { CustomerDetails } from "../helpers/printHelper.js";
+
+var customerDetails = new CustomerDetails();
+customerDetails.printFirstName("Timotej");
+customerDetails.printLastName("Pap");
