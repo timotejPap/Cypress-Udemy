9e2f11cec6037d17b82bf7dbc51f2814072e1458
Timotej Pap <paptimotej@gmail.com>
2025-09-05 10:44:38 +0200

    config, CLI

--- Cypress-Playground/cypress.config.js
+++ Cypress-Playground/cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
+    baseUrl: "https://playground.bondaracademy.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
 });
