d14d3224fb9d0dddc3d0cf2d7862c2d91434be4d
Timotej Pap <paptimotej@gmail.com>
2025-09-06 10:54:48 +0200

    DOM, locators

--- Cypress-Playground/cypress/e2e/firstTest.cy.js
+++ Cypress-Playground/cypress/e2e/firstTest.cy.js
@@ -2,12 +2,45 @@
 
 beforeEach("Open test app", () => {
     cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
 });
 
 //only tests
 it("Hello World", () => {
 
+    // locator by Tag name
+    cy.get("input");
+
+    // locator by ID value
+    cy.get("#inputEmail1");
+
+    // locator by Class value
+    cy.get(".input-full-width");
+
+    // locator by Attribute
+    cy.get("[fullwidth]");
+
+    // locator Attribute with value
+    cy.get('[placeholder="Email"]');
+
+    // entire class value
+    cy.get('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+
+    // locator by combination of Attribute
+    cy.get('[placeholder="Email"][fullwidth]')
+    cy.get('input[placeholder="Email"]')
+    
+    // find by data-cy attribute
+    cy.get('[data-cy="inputEmail1"]')
+
+
 });
+
+
+
+
+
 
 // it("Hello World2", () => {
 
