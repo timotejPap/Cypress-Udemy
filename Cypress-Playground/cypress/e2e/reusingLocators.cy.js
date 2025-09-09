8b34803d0a2fe58e79fc5bd3ef24f237c7655927
Timotej Pap <paptimotej@gmail.com>
2025-09-09 12:58:00 +0200

    alias + then

--- Cypress-Playground/cypress/e2e/reusingLocators.cy.js
+++ Cypress-Playground/cypress/e2e/reusingLocators.cy.js
@@ -15,11 +15,18 @@ it.only("Reusing locators", () => {
     // inputEmail1.parents("form").find("button")
     // inputEmail1.parents("form").find("nb-radio")
 
-    // 1. Cypress Alias
-    cy.get("#inputEmail1").as("inputEmail1") // create alias
-    cy.get("@inputEmail1").parents("form").find("button") // use alias
-    cy.get("@inputEmail1").parents("form").find("nb-radio")
+    // 1. Cypress ALIASING
+    // cy.get("#inputEmail1").as("inputEmail1") // create alias
+    // cy.get("@inputEmail1").parents("form").find("button") // use alias
+    // cy.get("@inputEmail1").parents("form").find("nb-radio")
 
-    // 2. Cypress then() method
-    cy.get("#inputEmail1").then
+    // 2. Cypress THEN() method
+    cy.get("#inputEmail1").then( $inputEmail => { // $inputEmail1 is a jQuery object)
+    cy.wrap($inputEmail).parents("form").find("button") // wrap() is used to convert jQuery object to Cypress object
+    cy.wrap($inputEmail).parents("form").find("nb-radio") // now we can use Cypress commands on it
+    cy.wrap("Hello").should("equal", "Hello") // we can also wrap strings, numbers, arrays, objects
+    cy.wrap($inputEmail).as("inputEmail2") // we can also create alias using wrap()
+    })
+        cy.get("@inputEmail2").parents("form").find("button")
+
 });
