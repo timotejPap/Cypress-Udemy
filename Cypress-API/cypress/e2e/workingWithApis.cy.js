49e615d239afde2e9b24e5236665cca257952baf
Timotej Pap <paptimotej@gmail.com>
2025-09-23 11:08:37 +0200

    mocking website url

--- Cypress-API/cypress/e2e/workingWithApis.cy.js
+++ Cypress-API/cypress/e2e/workingWithApis.cy.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
 it("API test", () => {
+  cy.intercept("GET", "**/tag", {
+    fixture: "tags.json",
+  });
+  cy.intercept("GET", "**/articles?limit=10&offset=0", {
+    fixture: "articles.json",
+  });
   cy.loginToApplication();
 });
