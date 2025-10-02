1711d06ff5e877fc229266c0c9f3fd8d35494d2f
Timotej Pap <paptimotej@gmail.com>
2025-10-02 09:26:18 +0200

    retries

--- Cypress-API/cypress.config.js
+++ Cypress-API/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    retries: 1, // retries once on failure
     baseUrl: "https://conduit.bondaracademy.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
